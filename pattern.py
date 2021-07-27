be0abb5239d8756335d69a0ead5d7580715841c4
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2021-07-27 19:11:16 +0700

    Update pattern

--- pattern.py
+++ pattern.py
@@ -25,31 +25,47 @@ def recognizer(sentence, pattern):
         if p is None: break
         idx = p.span()
         # print(p)
-        match_lst += [sent[k:][idx[0]:idx[1]]]
+        match = sent[k:][idx[0]:idx[1]]
+        match = re.sub('mot', '1', match)
+        match = re.sub('ruoi', ' 50 ', match)
+        match_lst += [match]
         k = k + idx[1]
     return match_lst
 
 ### COMMON PATTERN
 dd = r'(\d\d|\d)'
 ss = r'(\s|)'
-comma = r'(\.|,)'
-### WEIGHT
+dot = r'(\.|,)'
 kilo = r'(kg|ky|ki|can|kj|kilo|kilogam|k)'
 nang = r'((can|)(nang|nag))'
+met = r'(m|met)'
+
+### WEIGHT
 pt_weight_1 = r'\b\d\d{}{}'.format(ss, kilo) ### case 43
-pt_weight_2 = r'\d\d{}\d{}{}'.format(comma, ss, kilo) ### case 43.5
+pt_weight_2 = r'\d\d{}\d{}{}'.format(dot, ss, kilo) ### case 43.5
 pt_weight_3 = r'{}{}\d\d(\s\d|)({}|)'.format(nang, ss, kilo)
 pt_weight_4 = r'\d\d{}k{}cao+'.format(ss, ss)
 pt_weight = r'{}|{}|{}|{}'.format(pt_weight_1, pt_weight_2, pt_weight_3, pt_weight_4)
+
+
 ### HEIGHT
-pt_height_1 = r'\d\d\d{}cm'.format(ss)
-pt_height_6 = r'(\d|){}\d\d(?!{}{})|\b(\d|){}\d{}m\b'.format(comma, ss, kilo, comma, ss)
-pt_height_2 = r'(\d|)m{}\d\d(?!{}{})|\b(\d|)m{}\d\b'.format(ss, ss, kilo, ss)
-pt_height_3 = r'(cao+){}\d(\sm|{})*{}[m|met]*|(met){}{}'.format(ss, comma,dd, ss,dd)
-pt_height_4 = r'(\d{}{}|\d\d\d)(?={}(nang|nag))'.format(ss, dd, ss)
-pt_height_5 = r'(\d{}\d\d|\d\d\d)(?={}{})'.format(ss, ss, pt_weight)
-pt_height = r'({}|{}|{}|{}|{}|{})'.format(pt_height_1, \
-            pt_height_6, pt_height_3, pt_height_4, pt_height_5, pt_height_2)
+### 150cm, 150 cm 
+pt_height_1 = r'(cao+|)1\d\d{}cm'.format(ss)
+### 1m5, 1m50, 1 m 5, 1 m 50
+pt_height_2 = r'(cao+|)1{}{}{}{}'.format(ss, met, ss, dd)
+### 1.5m, 1 . 5m, 1.50m, 1 . 50m
+pt_height_3 = r'(cao+|)1{}{}{}{}m'.format(ss, dot, ss, dd)
+### 1.50 1,5 1 . 50 1 , 5 (not kg)
+pt_height_4 = r'(\b|cao+)1{}{}{}{}(?!{}{})'.format(ss, dot, ss, dd, ss, kilo)
+### m50, m5, m 50
+pt_height_5 = r'(\b|cao+){}{}{}\b'.format(met, ss, dd)
+### cao 150, 150 (nặng ...), 150 (45kg)
+pt_height_6 = r'cao+{}1\d\d|1\d\d\s(?=({}|{}))'.format(ss, nang, pt_weight)
+### 
+pt_height_7 = r'(met|m) ruoi'
+
+pt_height = r'({}|{}|{}|{}|{}|{}|{})'.format(pt_height_1, pt_height_2, \
+            pt_height_3, pt_height_4, pt_height_5, pt_height_6, pt_height_7)
 ###-------------------------------------------
 
 if __name__ == "__main__":
@@ -86,7 +102,8 @@ if __name__ == "__main__":
     for (d,p,i) in [(height_data[i],height[i], i) for i in range(len(height)) \
                                                 if height[i]!=height_data[i]]:
         # if re.search('cao|Cao',p) is not None: continue  
-        if normalize(d) == normalize(p): continue    
+        if normalize(d) == normalize(p): continue  
+        # if re.search('\d\d\d', d) is None: continue  
         # if re.search('m\d\d\d',d) is not None: continue                                   
         cnt+= 1
         print(text[i])
